d6030693aafecb8448883d0e42170303b1edf328
suyongming <476231418@qq.com>
2019-02-14 18:18:40 +0800

    openCV Demo

--- service-opencv/src/main/java/com/demo/OpenCVDemo.java
+++ service-opencv/src/main/java/com/demo/OpenCVDemo.java
@@ -1,6 +1,8 @@
 package com.demo;
 
+import com.util.ShowImage;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -14,10 +16,22 @@ public class OpenCVDemo {
         }
         public static void main(String[] args){
 
+//            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//            String imgPath = "E:\\图片\\表情\\xxydx.JPEG";
+//            Mat mat = Imgcodecs.imread(imgPath);
+//            ShowImage window = new ShowImage(mat);
+//            window.getFrame().setVisible(true);
+
+
+            Mat mat2 = Mat.eye(3,3, CvType.CV_8UC1);
+            System.out.println(mat2.dump());
+            Mat mat = Imgcodecs.imread("E:\\图片\\表情\\xxydx.jpg");
+            System.out.println(mat);
 
         }
 
-    //            Mat mat = Imgcodecs.imread("E:\\图片\\表情\\xxydx.jpg");
+
+
 //            String imgPath = "E:\\图片\\表情\\xxydx";
 //            Mat mat = Imgcodecs.imread(imgPath+".jpg");
 //            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -25,7 +39,6 @@ public class OpenCVDemo {
 //            Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);
 //            Imgproc.adaptiveThreshold(image, image, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 25, 10);
 //            Imgcodecs.imwrite(imgPath+"openCV.jpg", image);
-//            System.out.println(mat);
 //            System.out.println(mat);
 
 }
