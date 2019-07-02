18aa8c2e87cb40319b99e5b1a221f913861a5513
suyongming <476231418@qq.com>
2019-07-02 14:52:35 +0800

    cloud config git

--- service-conf/config-client/src/test/java/com/forezp/ConfigClientApplicationTests.java
+++ service-conf/config-client/src/test/java/com/forezp/ConfigClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.forezp.serviceoriginconf;
+package com.forezp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceOriginConfApplicationTests {
+public class ConfigClientApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
--- service-conf/config-server/src/main/java/com/forezp/ConfigServerApplication.java
+++ service-conf/config-server/src/main/java/com/forezp/ConfigServerApplication.java
@@ -2,15 +2,16 @@ package com.forezp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
-public class ServiceConfApplication {
+@RefreshScope
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceConfApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
